5cc3b58ae6b5eb0ab3c427c8171563375b782bac
SIRHERRY-LIM <herrylimherrylim@gail.com>
2021-08-15 22:30:43 +0800

    adding html and css styling

--- exercise1.php
+++ exercise1.php
@@ -3,3 +3,32 @@
 <?php
 
 $angka = [3, 4, 5, 6, 7, 8, 9];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+
+    <style>
+        div {
+
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div>3</div>
+</body>
+
+</html>
