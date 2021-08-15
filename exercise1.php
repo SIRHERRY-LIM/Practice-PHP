4d9916d0f54a23d4ddb9ead3cae66639ebdad6b9
SIRHERRY-LIM <herrylimherrylim@gail.com>
2021-08-15 23:42:21 +0800

    adding foreach into array

--- exercise1.php
+++ exercise1.php
@@ -2,7 +2,7 @@
 
 <?php
 
-$angka = [3, 4, 5, 6, 7, 8, 9];
+$angka = [3, 4, 5, 6, 7, 8, 9, 10];
 ?>
 
 <!DOCTYPE html>
@@ -15,20 +15,47 @@ $angka = [3, 4, 5, 6, 7, 8, 9];
     <title>Exercise 1</title>
 
     <style>
-        div {
+        .box {
 
             width: 50px;
             height: 50px;
             background-color: salmon;
             text-align: center;
             line-height: 50px;
+            margin: 3px;
+            float: left;
 
+        }
+
+        .clear {
+            clear: both;
         }
     </style>
 </head>
 
 <body>
-    <div>3</div>
+
+    <?php for ($i = 0; $i < count($angka); $i++) { ?>
+
+        <div class="box"><?php echo $angka[$i] ?></div>
+
+    <?php } ?>
+
+
+    <div class="clear"> </div>
+
+    <?php foreach ($angka as $a) { ?>
+        <div class="box"> <?php echo $a; ?> </div>
+    <?php } ?>
+
+
+    <div class="clear"> </div>
+
+    <?php foreach ($angka as $a) : ?>
+        <div class="box"> <?= $a; ?> </div>
+
+    <?php endforeach; ?>
+
 </body>
 
 </html>
